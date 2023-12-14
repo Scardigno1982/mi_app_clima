fa4829fd475d30acd7bf54ffae7f332b4ef57f52
Sergio Scardigno <sergioscardigno82@gmail.com>
2023-12-13 21:34:41 -0300

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,19 @@
 const request = require('supertest');
 const expect = require('chai').expect;
-const app = require('../server'); 
+const { app, server } = require('../server'); 
 
 describe('GET /clima', function() {
     it('Esta api responde con un json con el conteniendo el clima, para los muchachos del tp-10', function(done) {
-      request(app)
-        .get('/clima')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done); 
+        request(app)
+            .get('/clima')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done); 
     });
-  });
-  
+
+    after(() => {
+        if (server) {
+            server.close(); // Cerrar el servidor después de las pruebas
+        }
+    });
+});
